9d368d9833f0b04dbf2d02dde38c4ecc9513372e
1llionaires <paulchodev@gmail.com>
2022-06-14 17:15:13 -0700

    working counting stars-NASA API app

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-//Example fetch using pokemonapi.co
+document.querySelector("img").classList.add("hidden");
+document.querySelector("iframe").classList.add("hidden");
+
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
@@ -9,13 +11,18 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        if(data.media_type === 'image'){
-          document.querySelector('img').src = data.hdurl
-          document.querySelector('iframe').classList.toggle('hidden')
-        }else if(data.media_type === 'video'){
-          document.querySelector('iframe').src = data.url
+        if(data.media_type === "image"){
+          document.querySelector("img").src = data.hdurl;
+          document.querySelector("img").classList.remove("hidden");
+          document.querySelector("iframe").classList.add("hidden");
+        }else if(data.media_type === "video"){
+          document.querySelector("iframe").src = data.url;
+          document.querySelector("img").classList.add("hidden");
+          document.querySelector("iframe").classList.remove("hidden");
         }
         document.querySelector('h3').innerText = data.explanation
+        document.querySelector("h2").innerText = data.title;
+        document.querySelector(".credit").innerText = "by: "+data.copyright;
       })
       .catch(err => {
           console.log(`error ${err}`)
